e05a9fc3c1520cc7eac9ae06ec760c3f8789f3b8
Kartik <kaaro@Apples-MacBook-Pro.local>
2019-06-29 21:34:43 +0530

    fixed status

--- index.js
+++ index.js
@@ -16,18 +16,19 @@ client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
+    client.user.setActivity(`clips in ${client.guilds.size} servers`);    
 });
 
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving Clips in ${client.guilds.size} servers`);
+    client.user.setActivity(`clips in ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving Clips in ${client.guilds.size} servers`);
+    client.user.setActivity(`clips in ${client.guilds.size} servers`);
 });
 
 
